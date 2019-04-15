e00807906fd1aee726d7fd68a40007df545be14b
Lauren Forcey <lauren4c@gmail.com>
2019-04-15 08:51:38 -0600

    Completed Poker Rank Challenge - All tests pass.

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -1,9 +1,117 @@
 class PokerHand {
-  constructor() {}
-
+  constructor(hand) {
+    this.hand = hand;
+  }
   getRank() {
-    // Implement poker hand ranking
-    return 'Royal Flush';
+    var rank;
+    var suits = {
+      h: [],
+      s: [],
+      c: [],
+      d: []
+    };
+    var kinds = {
+      2: [],
+      3: [],
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+      8: [],
+      9: [],
+      T: [],
+      J: [],
+      Q: [],
+      K: [],
+      A: []
+    };
+    var countedKinds = [];
+
+    //sort cards by suit & by kind
+    const cards = this.hand.split(" ");
+    cards.forEach(function(card) {
+      var suit = card.split("")[1];
+      var kind = card.split("")[0];
+      kinds[kind].push(card);
+      suits[suit].push(card);
+    });
+
+    //let's find the rank!
+    while (!rank) {
+      for (var j in suits) {
+        if (suits.hasOwnProperty(j)) {
+          //check for flushes first by looking for groups of suits
+          if (suits[j].length == 5) {
+            if (longest_run() == 5) {
+              let checkForAce = suits[j].includes("A" + j);
+              checkForAce ? (rank = "Royal Flush") : (rank = "Straight Flush");
+              return rank;
+            } else {
+              return (rank = "Flush");
+            }
+          }
+        }
+      }
+      //check for straight
+      for (var k in kinds) {
+        if (kinds.hasOwnProperty(k)) {
+          if (longest_run() == 5) {
+            return (rank = "Straight");
+          }
+        }
+      }
+      //check for multiples/pairs
+      Object.keys(kinds).forEach(function(k) {
+        let length = kinds[k].length;
+        countedKinds.push(length);
+      });
+      if (countedKinds.includes(4)) {
+        return (rank = "Four of a Kind");
+      } else if (countedKinds.includes(3)) {
+        if (countedKinds.includes(2)) {
+          return (rank = "Full House");
+        } else {
+          return (rank = "Three of a Kind");
+        }
+      } else if (countedKinds.includes(2)) {
+        //check how many pairs
+        var pairs = {};
+        countedKinds.forEach(function(x) {
+          pairs[x] = (pairs[x] || 0) + 1;
+        });
+        if (pairs[2] == 2) {
+          return (rank = "Two Pair");
+        } else {
+          return (rank = "One Pair");
+        }
+      } else {
+        // if we haven't found a rank by now, it must be high card.
+        return (rank = "High Card");
+      }
+    }
+
+    //helper to check for runs
+    function longest_run() {
+      var count = 0;
+      var max_run = 0;
+      ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"].forEach(
+        function(k) {
+          if (kinds[k].length >= 1) {
+            count += 1;
+          } else {
+            if (count > max_run) {
+              max_run = count;
+            }
+            count = 0;
+          }
+        }
+      );
+      if (max_run > count) {
+        return max_run;
+      } else {
+        return count;
+      }
+    }
   }
 }
 
--- challenge-js/test/pokerHandTest.js
+++ challenge-js/test/pokerHandTest.js
@@ -1,47 +1,107 @@
-var assert = require('assert');
-var PokerHand = require('../pokerHand.js');
+var assert = require("assert");
+var PokerHand = require("../pokerHand.js");
 
 /**
  * test
  */
-describe('Rank a Royal Flush', function() {
-  it('Return royal flush when hand given', function() {
-    var hand = new PokerHand('As Ks Qs Js 10s');
-    assert.equal(hand.getRank(), 'Royal Flush');
+describe("Rank a Royal Flush", function() {
+  it("Return royal flush when hand given", function() {
+    var hand = new PokerHand("Ks Qs Js Ts As");
+    assert.equal(hand.getRank(), "Royal Flush");
   });
 });
 
 /**
  * test
  */
-describe('Rank a Pair', function() {
-  it('Return one pair when hand given', function() {
-    var hand = new PokerHand('Ah As 10c 7d 6s');
+describe("Rank A Straight Flush", function() {
+  var hand = new PokerHand("Th 9h 8h 7h 6h");
 
-    assert.equal(hand.getRank(), 'One Pair');
+  it("Return straight flush when hand given", function() {
+    assert.equal(hand.getRank(), "Straight Flush");
   });
 });
 
 /**
  * test
  */
-describe('Rank Two Pair', function() {
-  it('Return two pair when hand given', function() {
-    var hand = new PokerHand('Kh Kc 3s 3h 2d');
+describe("Rank A Four of a Kind", function() {
+  var hand = new PokerHand("6d 6s 6h 6c 9h");
 
-    assert.equal(hand.getRank(), 'Two Pair');
+  it("Return four of a kind when hand given", function() {
+    assert.equal(hand.getRank(), "Four of a Kind");
   });
 });
 
 /**
  * test
  */
-describe('Rank A Flush', function() {
-  var hand = new PokerHand('Kh Qh 6h 2h 9h');
+describe("Rank A Full House", function() {
+  var hand = new PokerHand("Th Td Ts 2h 2c");
 
-  it('Return flush when hand given', function() {
-    assert.equal(hand.getRank(), 'Flush');
+  it("Return Full House when hand given", function() {
+    assert.equal(hand.getRank(), "Full House");
+  });
+});
+/**
+ * test
+ */
+describe("Rank A Flush", function() {
+  var hand = new PokerHand("Kh Qh 6h 2h 9h");
+
+  it("Return flush when hand given", function() {
+    assert.equal(hand.getRank(), "Flush");
   });
 });
 
-// More tests go here
+/**
+ * test
+ */
+describe("Rank A Straight", function() {
+  var hand = new PokerHand("Th 9d 8c 7d 6h");
+
+  it("Return straight when hand given", function() {
+    assert.equal(hand.getRank(), "Straight");
+  });
+});
+
+/**
+ * test
+ */
+describe("Rank a three of a kind", function() {
+  it("Return three of a kind when hand given", function() {
+    var hand = new PokerHand("Ah As Ad 7c 6s");
+
+    assert.equal(hand.getRank(), "Three of a Kind");
+  });
+});
+/**
+ * test
+ */
+describe("Rank a Two Pair", function() {
+  it("Return two pair when hand given", function() {
+    var hand = new PokerHand("Ah As Tc Ts 6s");
+
+    assert.equal(hand.getRank(), "Two Pair");
+  });
+});
+/**
+ * test
+ */
+describe("Rank a Pair", function() {
+  it("Return one pair when hand given", function() {
+    var hand = new PokerHand("Ah As Tc 7d 6s");
+
+    assert.equal(hand.getRank(), "One Pair");
+  });
+});
+/**
+ * test
+ */
+describe("Rank a high card", function() {
+  it("Return high card when hand given", function() {
+    var hand = new PokerHand("Kh Qs Tc 7d 6s");
+
+    assert.equal(hand.getRank(), "High Card");
+  });
+});
